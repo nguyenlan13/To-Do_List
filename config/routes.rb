9e0fe069e6cfb4e6ca57a2af99c4b7926f5a9e4e
Lan <nguyenlan13@gmail.com>
2021-10-25 02:33:24 -0700

    add root

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :lists do
      resources :items
+     get "view_by_date/:date/items" => "items#view_by_date", as: :view_by_date
   end
 
   resources :items 
 
-  get "view_by_date/:date/items" => "items#view_by_date"
+  # get "view_by_date/:date/items" => "items#view_by_date", as: :view_by_date
 
+  root 'lists#new'
 end
