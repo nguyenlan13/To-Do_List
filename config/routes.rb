b959f9c14a82c3045bb7fdfe5e2cfb2c25831c78
Lan <nguyenlan13@gmail.com>
2021-10-24 19:46:19 -0700

    add nested route for lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists
+  resources :lists do
+     resources :items
+  end
+
   resources :items 
+
+  get "view_by_date/:date/items" => "items#view_by_date"
+
 end
