d16c0e82c77cb2c396b93c76a7f50452f23eb40e
Lan <nguyenlan13@gmail.com>
2021-10-25 03:36:57 -0700

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+grocery = List.create(title: "grocery", category: "Personal")
+programming = List.create(title: "programming", category: "Professional")
+gifts = List.create(title: "gifts", category: "Personal")
+
+sister = Item.create(list: gifts, name: "sister", notes: "", date: "2021-10-25 08:38:00", completed: true)
+brother = Item.create(list: gifts, name: "brother", notes: "", date: "2021-10-27 08:38:00", completed: true)
+aunt = Item.create(list: gifts, name: "aunt", notes: "", date: "2021-10-24 08:38:00", completed: false)
+uncle = Item.create(list: gifts, name: "uncle", notes: "", date: "2021-10-25 08:38:00", completed: false)
+
+
